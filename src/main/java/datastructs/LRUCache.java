520b52e068839e64961b688124da333f150fff2d
Dom <domreyes13@protonmail.com>
2017-09-01 15:37:28 -0700

    added LRU cache tests

--- src/main/java/datastructs/LRUCache.java
+++ src/main/java/datastructs/LRUCache.java
@@ -27,22 +27,66 @@ public class LRUCache<K, V> {
 	private Node head, tail;
 
 	public LRUCache(int capacity) {
+		cache = new HashMap<>();
 		this.capacity = capacity;
 		count = 0;
 		cache = new HashMap<>();
 		head = new Node();
 		tail = new Node();
+		head.next = tail;
+		tail.prev = head;
 	}
-	
+
 	public int size() {
 		return count;
 	}
-	
-    public int get(K key) {
-        return 0;
-    }
-    
-    public void put(K key, V value) {
-        
-    }
+
+	public V get(K key) {
+		Node node = cache.get(key);
+		if (node != null) {
+			removeNode(node);
+			addToHead(node);
+			return node.val;
+		} else {
+			return null;
+		}
+	}
+
+	public void put(K key, V value) {
+		Node node = cache.get(key);
+
+		if (node == null) {
+			Node newNode = new Node(key, value);
+			cache.put(key, newNode);
+			addToHead(newNode);
+			count++;
+		} else {
+			node.val = value;
+			removeNode(node);
+			addToHead(node);
+		}
+
+		if (count > capacity) {
+			Node end = tail.prev;
+			removeNode(end);
+			cache.remove(end.key);
+			count--;
+		}
+	}
+
+	private void removeNode(Node node) {
+		if (count < 3)
+			return;
+
+		node.next.prev = node.prev;
+		node.prev.next = node.next;
+	}
+
+	private void addToHead(Node node) {
+		Node oldHead = head.next;
+		oldHead.prev = node;
+		node.next = oldHead;
+		node.prev = head;
+		head.next = node;
+	}
 }
--- src/test/java/datastructs/LRUCacheTest.java
+++ src/test/java/datastructs/LRUCacheTest.java
@@ -16,4 +16,68 @@ public class LRUCacheTest {
 		
 		Assert.assertEquals(0, size);
 	}
+	
+	@Test
+	public void getReturnsValue() {
+		int capacity = 4;
+		LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer>(capacity);
+		
+		cache.put(1, 10);
+		cache.put(2, 20);
+		cache.put(3, 30);
+		int res =  cache.get(2);
+		
+		Assert.assertEquals(20, res);	
+	}
+	
+	@Test
+	public void putUpdatesExistingKeyValue() {
+		int capacity = 20;
+		LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer>(capacity);
+		
+		cache.put(1, 4);
+		cache.put(1, 6);
+		
+		int size = cache.size();
+		int val = cache.get(1);
+		
+		Assert.assertEquals(1, size);
+		Assert.assertEquals(6, val);
+	}
+	
+	@Test
+	public void getReturnsNullOnNoFind() {
+		int capacity = 20;
+		LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer>(capacity);
+		
+		cache.put(1, 4);
+		cache.put(1, 6);
+		
+		Integer res = cache.get(2);
+		
+		Assert.assertNull(res);
+	}
+	
+	@Test
+	public void putRemovesOldestAtCapacity() {
+		int capacity = 4;
+		LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer>(capacity);
+		
+		cache.put(1, 10);
+		cache.put(2, 20);
+		cache.put(3, 30);
+		cache.put(4, 40);
+		cache.get(1);
+		cache.get(3);
+		cache.get(4);
+		cache.get(1);
+		cache.put(5, 50); // should remove 2
+		
+		int size = cache.size();
+		Assert.assertEquals(4, size);
+		
+		Integer res = cache.get(2);
+		Assert.assertNull(res);	
+	}
+	
 }
