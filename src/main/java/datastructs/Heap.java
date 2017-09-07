06b65fa567a020420d16bb05a83464d68344c122
Dom <domreyes13@protonmail.com>
2017-09-06 20:28:50 -0700

    Add heap tests.

--- src/main/java/datastructs/Heap.java
+++ src/main/java/datastructs/Heap.java
@@ -1,71 +1,110 @@
 package datastructs;
 
-import java.util.Comparator;
 import java.util.List;
 
-public class Heap<T> {
+public class Heap<T extends Comparable<T>> {
 
 	private enum Resize {
 		GROW, SHRINK
 	}
 
-	private Comparator<T> _comp;
 	private int _count;
 	private T[] _data;
 	private final int INITSIZE = 8;
+	private final double SHRINK_THRESHOLD = 0.25;
+	private final int RESIZE_MULT = 2;
 
 	@SuppressWarnings("unchecked")
-	public Heap(Comparator<T> comp) {
-		this._comp = comp;
+	public Heap() {
 		_count = 0;
-		_data = (T[]) new Object[INITSIZE];
+		_data = (T[]) new Comparable[INITSIZE];
 	}
 
 	public int size() {
 		return _count;
 	}
 
+	public boolean empty() {
+		return _count == 0;
+	}
+
 	public void clear() {
 		_count = 0;
 		_data = null;
-		_comp = null;
 	}
 
-	public T top() {
-		if (_count > 0)
-			return _data[0];
-		else
-			return null;
-	}
+	@SuppressWarnings("unchecked")
+	public void _heapify(List<T> data) {
+		if (data == null)
+			return;
 
-	public T get() {
-		if (_count > 0) {
-			_count--;
-			siftUp();
-			
-			if(0.25 * _data.length > _count)
-				resize(Resize.SHRINK);
-			
-			return _data[0];
-		} else
-			return null;
-	}
-
-	public void heapify(List<T> data) {
 		_count = data.size();
+		_data = (T[]) new Comparable[_count];
+
+		for (int i = 0; i < _count; i++)
+			_data[i] = data.get(i);
+
+		for (int i = (_count / 2) - 1; i >= 0; i--)
+			siftDown(i);
 	}
 
 	public void add(T val) {
-		
+		if (_count == _data.length)
+			resize(Resize.GROW);
+
+		_data[_count] = val;
+		siftUp(_count);
 		_count++;
 	}
 
-	private void siftUp() {
-
+	public T top() {
+		if (_count < 1)
+			return null;
+		else
+			return _data[0];
 	}
 
-	private void siftDown() {
+	public T removeTop() {
+		if (_count < 1)
+			return null;
 
+		T top = _data[0];
+		_data[0] = _data[--_count];
+		siftDown(0);
+
+		if (SHRINK_THRESHOLD * _data.length > _count && _count > INITSIZE * RESIZE_MULT)
+			resize(Resize.SHRINK);
+
+		return top;
+	}
+
+	private void siftUp(int idx) {
+		while (idx > 0 && _data[idx].compareTo(_data[(idx - 1) / 2]) < 0) {
+			swap(idx, (idx - 1) / 2);
+			idx = (idx - 1) / 2;
+		}
+	}
+
+	private void siftDown(int node) {
+		while (2 * node + 1 < _count) {
+			int child = 2 * node + 1;
+			int largerChild = child;
+
+			if (child + 1 < _count && _data[child + 1].compareTo(_data[child]) < 0)
+				largerChild = child + 1;
+
+			if (_data[largerChild].compareTo(_data[node]) < 0) {
+				swap(node, largerChild);
+				node = largerChild;
+			} else
+				node = _count;
+		}
+	}
+
+	private void swap(int idx1, int idx2) {
+		T tmp = _data[idx1];
+		_data[idx1] = _data[idx2];
+		_data[idx2] = tmp;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -73,9 +112,9 @@ public class Heap<T> {
 		T[] newData;
 
 		if (direction == Resize.GROW)
-			newData = (T[]) new Object[_data.length * 2];
+			newData = (T[]) new Object[_data.length * RESIZE_MULT];
 		else
-			newData = (T[]) new Object[_data.length / 2];
+			newData = (T[]) new Object[_data.length / RESIZE_MULT];
 
 		for (int i = 0; i < _data.length; i++) {
 			newData[i] = _data[i];
--- src/test/java/datastructs/HeapTest.java
+++ src/test/java/datastructs/HeapTest.java
@@ -3,6 +3,120 @@ package datastructs;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.ArrayList;
+
 public class HeapTest {
-	
+
+	@Test
+	public void constructorInitSize() {
+
+		Heap<Integer> heap = new Heap<>();
+
+		int size = heap.size();
+		boolean empty = heap.empty();
+		Assert.assertTrue(empty);
+		Assert.assertEquals(0, size);
+	}
+
+	@Test
+	public void heapifyWithNull() {
+
+		Heap<Integer> heap = new Heap<>();
+
+		heap._heapify(null);
+
+		int size = heap.size();
+		boolean empty = heap.empty();
+		Assert.assertTrue(empty);
+		Assert.assertEquals(0, size);
+	}
+
+	@Test
+	public void clearResetsHeap() {
+		Heap<Integer> heap = new Heap<>();
+		ArrayList<Integer> data = new ArrayList<>();
+
+		data.add(2);
+		data.add(4);
+		data.add(-31);
+		data.add(8);
+		data.add(-23);
+		data.add(13);
+
+		heap.clear();
+
+		Integer top = heap.top();
+		Assert.assertNull(top);
+
+		int size = heap.size();
+		Assert.assertEquals(0, size);
+
+		boolean empty = heap.empty();
+		Assert.assertTrue(empty);
+	}
+
+	@Test
+	public void topIsMin() {
+		Heap<Integer> heap = new Heap<>();
+		ArrayList<Integer> data = new ArrayList<>();
+
+		data.add(2);
+		data.add(4);
+		data.add(-31);
+		data.add(8);
+		data.add(-23);
+		data.add(13);
+
+		heap._heapify(data);
+
+		int top = heap.removeTop();
+
+		Assert.assertEquals(-31, top);
+
+		int size = heap.size();
+		Assert.assertEquals(5, size);
+	}
+
+	@Test
+	public void addInsertsWithPriority() {
+		Heap<Integer> heap = new Heap<>();
+
+		heap.add(2);
+		heap.add(4);
+		heap.add(-31);
+		heap.add(8);
+		heap.add(-23);
+		heap.add(13);
+
+		Integer min = Integer.MIN_VALUE;
+		while (!heap.empty()) {
+			Assert.assertTrue(min < heap.top());
+			min = heap.removeTop();
+		}
+	}
+
+	@Test
+	public void heapifyInsertsWithPriority() {
+		Heap<Integer> heap = new Heap<>();
+		ArrayList<Integer> data = new ArrayList<>();
+
+		data.add(2);
+		data.add(4);
+		data.add(-31);
+		data.add(8);
+		data.add(-23);
+		data.add(13);
+
+		heap._heapify(data);
+
+		Integer min = Integer.MIN_VALUE;
+		while (!heap.empty()) {
+			Assert.assertTrue(min < heap.top());
+			min = heap.removeTop();
+		}
+
+		int size = heap.size();
+		Assert.assertEquals(0, size);
+	}
+
 }
